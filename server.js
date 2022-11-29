e9b3ea50210ac7e2f7bdb669e26e7aa738cbeccd
Vivek Gude <59313903+vivekgude@users.noreply.github.com>
2022-11-29 21:59:19 +0530

    update

--- server.js
+++ server.js
@@ -32,3 +32,11 @@ mongoose
   .catch(error => {
     console.log(error);
   });
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static('frontend/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
